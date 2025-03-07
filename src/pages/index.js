ffc415d8a251f6d7e12dc2113bd2f7addcf9e07b
Ali Akbar <sayaliakbar@gmail.com>
2025-03-08 01:24:38 +0500

    Add index file to export page components: About, Contact, Home, NFT, and Roadmap

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import About from "./About/About.jsx";
+import Contact from "./Contact/Contact.jsx";
+import Home from "./Home/Home.jsx";
+import NFT from "./NFT/NFT.jsx";
+import Roadmap from "./Roadmap/Roadmap.jsx";
+
+export { About, Contact, Home, NFT, Roadmap };
