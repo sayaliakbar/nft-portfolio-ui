0c9decaa474b8421ffe0140fcd82e7f4d5c4cdfd
Ali Akbar <sayaliakbar@gmail.com>
2025-03-09 00:41:34 +0500

    Add Home section layout and styling with milestones and buttons

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,63 @@
 import React from "react";
 
 import "./home.css";
+import { Heading, Button } from "../../components";
+import sixfaces01 from "../../assets/images/sixfaces01.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="gradient__bg-home gradient__bg-blue " />
+      <div className="yorfy__home section__margin">
+        <div className="yorfy__home-leftcolumn">
+          <Heading
+            heading="Welcome to Yorfy"
+            title="Now Available, Meet Yorfy NFT Collection ⭐️"
+            body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          />
+          <div className="yorfy__home-leftcolumn__milestones">
+            <div>
+              <h3>546</h3>
+              <p>NFT Items</p>
+            </div>
+            <div className="yorfy__home-leftcolumn__milestones-divider" />
+            <div>
+              <h3>42</h3>
+              <p>Owners</p>
+            </div>
+            <div className="yorfy__home-leftcolumn__milestones-divider" />
+            <div>
+              <h3>378</h3>
+              <p>Items Sold</p>
+            </div>
+          </div>
+        </div>
+        <div className="yorfy__home-rightcolumn">
+          <div className="yorfy__home-rightcolumn__image">
+            <img src={sixfaces01} alt="Six Faces" />
+          </div>
+          <div className="yorfy__home-rightcolumn__content">
+            <Button
+              text="Buy an OpenSea"
+              type="primary"
+              size="large"
+              onClick={console.log("clicked")}
+              disabled={false}
+            />
+            <Button />
+            <Button
+              text="Know More"
+              type="secondary"
+              size="large"
+              onClick={console.log("clicked")}
+              disabled={false}
+            />
+            <Button />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
