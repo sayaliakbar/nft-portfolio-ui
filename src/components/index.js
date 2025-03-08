46c7e0f2878ec1274dd017e0cc28dc4eb21024c8
Ali Akbar <sayaliakbar@gmail.com>
2025-03-09 00:40:32 +0500

    Add Heading component to exports in index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Button from "./Button/Button.jsx";
 import Footer from "./Footer/Footer.jsx";
 import Navbar from "./Navbar/Navbar.jsx";
+import Heading from "./Heading/Heading.jsx";
 
-export { Button, Footer, Navbar };
+export { Button, Footer, Navbar, Heading };
