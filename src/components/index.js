f54a8a00ae827a09f0ba10f35ff393b9df39c88a
Ali Akbar <sayaliakbar@gmail.com>
2025-03-09 11:50:20 +0500

    Add UserCard component to index export for improved component accessibility

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer.jsx";
 import Navbar from "./Navbar/Navbar.jsx";
 import Heading from "./Heading/Heading.jsx";
 import SaleBar from "./SaleBar/SaleBar.jsx";
+import UserCard from "./UserCard/UserCard.jsx";
 
-export { Button, Footer, Navbar, Heading, SaleBar };
+export { Button, Footer, Navbar, Heading, SaleBar, UserCard };
