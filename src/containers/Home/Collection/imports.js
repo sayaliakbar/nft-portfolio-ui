d11e29a4475da9c334523319487823ef33b29327
Ali Akbar <sayaliakbar@gmail.com>
2025-03-09 11:24:33 +0500

    Add image and logo imports for Collection component

---
+++ src/containers/Home/Collection/imports.js
@@ -0,0 +1,6 @@
+import sixfaces03 from "../../../assets/images/sixfaces03.png";
+import sixfaces04 from "../../../assets/images/sixfaces04.png";
+import sixfaces05 from "../../../assets/images/sixfaces05.png";
+import logo from "../../../assets/logo.svg";
+
+export { sixfaces03, sixfaces04, sixfaces05, logo };
