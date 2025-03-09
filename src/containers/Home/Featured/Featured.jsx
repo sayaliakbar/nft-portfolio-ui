f2269e01201f9a04d801edbe23e97aa402c938d2
Ali Akbar <sayaliakbar@gmail.com>
2025-03-09 08:10:33 +0500

    Add Featured component with heading and layout structure

--- src/containers/Home/Featured/Featured.jsx
+++ src/containers/Home/Featured/Featured.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./featured.css";
 
+import { Heading } from "../../../components/index.js";
+
 const Featured = () => {
-  return <div>Featured</div>;
+  return (
+    <div className="yorfy__home__featured section__margin">
+      <div className="yorfy__home__featured-heading">
+        <Heading
+          heading="Featured"
+          title="Hot Trending On This Week from Yorfy"
+          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+      </div>
+      <div className="yorfy__home__featured-container">
+        <div className="yorfy__home__featured-container__content"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
