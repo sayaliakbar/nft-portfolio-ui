27361cf549ea5bfd762a30708b455a5fb9c71841
Ali Akbar <sayaliakbar@gmail.com>
2025-03-09 07:41:49 +0500

    Update SaleBar component to enhance layout and display discount information

--- src/components/SaleBar/SaleBar.jsx
+++ src/components/SaleBar/SaleBar.jsx
@@ -1,5 +1,14 @@
+import "./salebar.css";
+
 const SaleBar = () => {
-  return <div>SaleBar</div>;
+  return (
+    <div className="yorfy__home__salebar">
+      <h4>Discount Sale</h4>
+      <h4 className="yorfy__home__salebar-outline">Up to 40%</h4>
+      <h4>Discount Sale</h4>
+      <h4 className="yorfy__home__salebar-outline">Up to 40%</h4>
+    </div>
+  );
 };
 
 export default SaleBar;
