8dc2f953074aa3fad5dffc59474dadc068f4fd19
Ali Akbar <sayaliakbar@gmail.com>
2025-03-09 15:30:04 +0500

    Add imports for social media icons and company logo in Footer component

---
+++ src/components/Footer/imports.js
@@ -0,0 +1,7 @@
+import facebook from "../../assets/icons/facebook.svg";
+import instagram from "../../assets/icons/instagram.svg";
+import linkedin from "../../assets/icons/linkedin.svg";
+import youtube from "../../assets/icons/youtube.svg";
+import company from "../../assets/logo.svg";
+
+export { facebook, instagram, linkedin, youtube, company };
