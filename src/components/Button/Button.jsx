d6af1ccaf9fc80f780ba032711ac1d276ae6ff48
Ali Akbar <sayaliakbar@gmail.com>
2025-03-09 14:46:17 +0500

    Enhance Button component to support icon display and style prop

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 
 import "./button.css";
+import done from "../../assets/icons/done.svg";
 
-const Button = ({ text, type, size, onClick, disabled }) => {
+const Button = ({
+  text,
+  style,
+  size,
+  onClick,
+  disabled = false,
+  icon = false,
+}) => {
   return (
     <button
-      className={`yorfy__btn yorfy__btn-${type} yorfy__btn-${size}`}
+      className={`yorfy__btn yorfy__btn-${style} yorfy__btn-${size}`}
       onClick={onClick}
       disabled={disabled}
     >
       {text}
+      {icon && <img src={done} alt="done" />}
     </button>
   );
 };
