036899e77b473901067e1e9a7d007a216d338103
Ali Akbar <sayaliakbar@gmail.com>
2025-03-09 06:54:48 +0500

    Add MainLayout component with styling and structure for layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
 import { Navbar, Footer } from "../components";
+import "./MainLayout.css";
 
 const MainLayout = ({ children }) => {
   return (
-    <>
+    <div className="yorfy">
       <Navbar />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
