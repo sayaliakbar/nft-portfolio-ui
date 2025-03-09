e503f2ef3f6ff691cff987239a07380d830a37a3
Ali Akbar <sayaliakbar@gmail.com>
2025-03-09 06:53:07 +0500

    Add exports for Home container components in index.js

---
+++ src/containers/Home/index.js
@@ -0,0 +1,17 @@
+import Gradients from "./Gradients/Gradients.jsx";
+import Header from "./Header/Header.jsx";
+import Featured from "./Featured/Featured.jsx";
+import Collection from "./Collection/Collection.jsx";
+import Community from "./Community/Community.jsx";
+import Collaboration from "./Collaboration/Collaboration.jsx";
+import Newsletter from "./Newsletter/Newsletter.jsx";
+
+export {
+  Gradients,
+  Header,
+  Featured,
+  Collection,
+  Community,
+  Collaboration,
+  Newsletter,
+};
