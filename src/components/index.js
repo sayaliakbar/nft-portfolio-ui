dc3015a1884c5dbcfceebbe9f9bc2026b10db6ec
Ali Akbar <sayaliakbar@gmail.com>
2025-03-09 14:46:04 +0500

    Add Input component to exports for enhanced functionality

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import Navbar from "./Navbar/Navbar.jsx";
 import Heading from "./Heading/Heading.jsx";
 import SaleBar from "./SaleBar/SaleBar.jsx";
 import UserCard from "./UserCard/UserCard.jsx";
+import Input from "./Input/Input.jsx";
 
-export { Button, Footer, Navbar, Heading, SaleBar, UserCard };
+export { Button, Footer, Navbar, Heading, SaleBar, UserCard, Input };
