270ae0afb92c122c3ab4efb17e2b6c7c4e847de1
Ali Akbar <sayaliakbar@gmail.com>
2025-03-09 13:17:35 +0500

    Add brand logo imports for collaboration section

---
+++ src/containers/Home/Collaboration/import.js
@@ -0,0 +1,6 @@
+import logoipsum01 from "../../../assets/brands/logoipsum01.png";
+import logoipsum02 from "../../../assets/brands/logoipsum02.png";
+import logoipsum03 from "../../../assets/brands/logoipsum03.png";
+import logoipsum04 from "../../../assets/brands/logoipsum04.png";
+
+export { logoipsum01, logoipsum02, logoipsum03, logoipsum04 };
