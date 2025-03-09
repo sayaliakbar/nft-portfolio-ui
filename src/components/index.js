8004a1425a196ac048975655f8983078aaa59c40
Ali Akbar <sayaliakbar@gmail.com>
2025-03-09 06:52:43 +0500

    Add SaleBar component to exports in index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Button from "./Button/Button.jsx";
 import Footer from "./Footer/Footer.jsx";
 import Navbar from "./Navbar/Navbar.jsx";
 import Heading from "./Heading/Heading.jsx";
+import SaleBar from "./SaleBar/SaleBar.jsx";
 
-export { Button, Footer, Navbar, Heading };
+export { Button, Footer, Navbar, Heading, SaleBar };
