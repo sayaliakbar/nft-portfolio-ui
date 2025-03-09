0d616cf5490b3f9177c1f6a8315b522abc044bdc
Ali Akbar <sayaliakbar@gmail.com>
2025-03-09 14:47:19 +0500

    Implement Newsletter component with heading and email submission form

--- src/containers/Home/Newsletter/Newsletter.jsx
+++ src/containers/Home/Newsletter/Newsletter.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import "./newsletter.css";
 
+import { Heading, Button, Input } from "../../../components";
+
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  return (
+    <div className="yorfy__newsletter section__margin">
+      <div className="yorfy__newsletter-heading">
+        <Heading
+          heading="Newsletter"
+          title="You Do Not Want to Miss Out on this!"
+          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+      </div>
+      <form
+        className="yorfy__newsletter-input"
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log("submitted");
+        }}
+      >
+        <Input placeholder="Your Email" size="large" />
+        <Button
+          text="Submit"
+          type="submit"
+          style="primary"
+          size="large"
+          icon={false}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Newsletter;
