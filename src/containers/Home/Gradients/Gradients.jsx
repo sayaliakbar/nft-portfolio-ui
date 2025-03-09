87cb96cfbc631aae04169d999b0479e2ed169222
Ali Akbar <sayaliakbar@gmail.com>
2025-03-09 06:54:33 +0500

    Add Gradients component with styles for home layout

---
+++ src/containers/Home/Gradients/Gradients.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./gradients.css";
+
+const Gradients = () => {
+  return (
+    <>
+      <div className="yorfy__home-gradient-leftColumn gradient__bg-blue" />
+      <div className="yorfy__home-gradient-mid gradient__bg-purple" />
+      <div className="yorfy__home-gradient-rightColumn gradient__bg-blue" />
+    </>
+  );
+};
+
+export default Gradients;
