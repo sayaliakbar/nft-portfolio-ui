ec5e73098c63b12ff0d2c61dcf0a618d261c9c80
Ali Akbar <sayaliakbar@gmail.com>
2025-03-10 08:24:54 +0500

    Add NFT header gradients and integrate Heading component for enhanced presentation

--- src/pages/NFT/NFT.jsx
+++ src/pages/NFT/NFT.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
 import "./nft.css";
+import { Heading } from "../../components/index.js";
 
 const NFT = () => {
-  return <div>NFT</div>;
+  return (
+    <>
+      <div className="yorfy__nft-header__gradient-purple gradient__bg-purple" />
+      <div className="yorfy__nft-header__gradient-blue gradient__bg-blue" />
+      <div className="yorfy__nft">
+        <Heading
+          title="Our Collection"
+          heading="NFT"
+          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+      </div>
+    </>
+  );
 };
 
 export default NFT;
