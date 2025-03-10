2c97f22325bd24f5faf1f7b134ac79442f2c35ab
Ali Akbar <sayaliakbar@gmail.com>
2025-03-10 10:42:26 +0500

    Add image imports for sixfaces assets in Home page

---
+++ src/pages/Home/imports.js
@@ -0,0 +1,5 @@
+import sixfaces01 from "../../assets/images/sixfaces01.png";
+import sixfaces02 from "../../assets/images/sixfaces02.png";
+import sixfaces03 from "../../assets/images/sixfaces03.png";
+
+export { sixfaces01, sixfaces02, sixfaces03 };
