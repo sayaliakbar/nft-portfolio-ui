420602eb1fee69bf017efff599ae0fd4faff5684
Ali Akbar <sayaliakbar@gmail.com>
2025-03-10 10:40:55 +0500

    Rename Gradients import to HomeBgGradients for clarity

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,16 +1,14 @@
-import Gradients from "./Gradients/Gradients.jsx";
+import HomeBgGradients from "./Gradients/HomeBgGradients.jsx";
 import Header from "./Header/Header.jsx";
 import Featured from "./Featured/Featured.jsx";
-import Collection from "./Collection/Collection.jsx";
 import Community from "./Community/Community.jsx";
 import Collaboration from "./Collaboration/Collaboration.jsx";
 import Newsletter from "./Newsletter/Newsletter.jsx";
 
 export {
-  Gradients,
+  HomeBgGradients,
   Header,
   Featured,
-  Collection,
   Community,
   Collaboration,
   Newsletter,
