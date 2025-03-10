1e179c3e67cf756d3ef339d8c8d3d057bccb3881
Ali Akbar <sayaliakbar@gmail.com>
2025-03-10 10:41:34 +0500

    Add HomeBgGradients component for background gradient display

---
+++ src/containers/Home/Gradients/HomeBgGradients.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./homebggradients.css";
+
+const Gradients = () => {
+  return (
+    <>
+      <div className="yorfy__home-gradient-leftColumn gradient__bg-blue" />
+      <div className="yorfy__home-gradient-mid gradient__bg-purple" />
+      <div className="yorfy__home-gradient-rightColumn gradient__bg-blue" />
+      <div className=" yorfy__home-collection-gradientBlue gradient__bg-blue" />
+      <div className=" yorfy__home-collection-gradientPurple gradient__bg-purple" />
+    </>
+  );
+};
+
+export default Gradients;
