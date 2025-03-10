dbe32c688d27c8caed26d0c11cb42ff6600f2dd3
Ali Akbar <sayaliakbar@gmail.com>
2025-03-11 03:20:30 +0500

    Add logo import to Home page assets

--- src/pages/Home/imports.js
+++ src/pages/Home/imports.js
@@ -1,5 +1,6 @@
 import sixfaces01 from "../../assets/images/sixfaces01.png";
 import sixfaces02 from "../../assets/images/sixfaces02.png";
 import sixfaces03 from "../../assets/images/sixfaces03.png";
+import logo from "../../assets/logo.svg";
 
-export { sixfaces01, sixfaces02, sixfaces03 };
+export { sixfaces01, sixfaces02, sixfaces03, logo };
