95570bd18064d4b6474e05aa655b7c72f6520d20
Ali Akbar <sayaliakbar@gmail.com>
2025-03-10 22:32:36 +0500

    Add image imports for sixfaces assets in Roadmap Content

---
+++ src/containers/Roadmap/Content/imports.js
@@ -0,0 +1,7 @@
+import sixfaces01 from "../../../assets/images/sixfaces01.png";
+import sixfaces02 from "../../../assets/images/sixfaces02.png";
+import sixfaces03 from "../../../assets/images/sixfaces03.png";
+import sixfaces04 from "../../../assets/images/sixfaces04.png";
+import sixfaces05 from "../../../assets/images/sixfaces05.png";
+
+export { sixfaces01, sixfaces02, sixfaces03, sixfaces04, sixfaces05 };
