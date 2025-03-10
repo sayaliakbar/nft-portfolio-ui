8e66e25df2c9bf145ba62f6cc50c3be1e451aafd
Ali Akbar <sayaliakbar@gmail.com>
2025-03-10 10:40:39 +0500

    Add NFT collection display with data and integrate background gradients

--- src/pages/NFT/NFT.jsx
+++ src/pages/NFT/NFT.jsx
@@ -1,19 +1,50 @@
 import React from "react";
 
 import "./nft.css";
-import { Heading } from "../../components/index.js";
+import { Heading, Collection } from "../../components/index.js";
+
+import { NftBgGradients } from "../../containers/NFT/index.js";
+
+import {
+  sixfaces01,
+  sixfaces02,
+  sixfaces03,
+  sixfaces04,
+  sixfaces05,
+} from "./imports.js";
+
+const nftCollectionData = [
+  { title: "YorNoose #432", imageSource: sixfaces01 },
+  { title: "YorHayr #332", imageSource: sixfaces02 },
+  { title: "YorMwoth #765", imageSource: sixfaces03 },
+  {
+    title: "YorTyth #646",
+    imageSource: sixfaces04,
+  },
+  { title: "YorLyps #167", imageSource: sixfaces05 },
+  { title: "YorEyes #234", imageSource: sixfaces01 },
+  { title: "YorTwong #912", imageSource: sixfaces02 },
+  { title: "YorCray #348", imageSource: sixfaces03 },
+  {
+    title: "YorErpon #966",
+    imageSource: sixfaces04,
+  },
+  { title: "YorLup #521", imageSource: sixfaces05 },
+  { title: "YorFlo #730", imageSource: sixfaces01 },
+  { title: "YorSmyl #309", imageSource: sixfaces02 },
+];
 
 const NFT = () => {
   return (
     <>
-      <div className="yorfy__nft-header__gradient-purple gradient__bg-purple" />
-      <div className="yorfy__nft-header__gradient-blue gradient__bg-blue" />
+      <NftBgGradients />
       <div className="yorfy__nft">
         <Heading
           title="Our Collection"
           heading="NFT"
           body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
+        <Collection nftCollectionData={nftCollectionData} />
       </div>
     </>
   );
