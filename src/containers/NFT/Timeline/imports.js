8ccf5d8f36e7e1f873bb5c56d0804c5230f970b6
Ali Akbar <sayaliakbar@gmail.com>
2025-03-10 13:52:11 +0500

    Add icon imports for percentage indicators in Timeline component

---
+++ src/containers/NFT/Timeline/imports.js
@@ -0,0 +1,7 @@
+import percent20 from "../../../assets/icons/number20.svg";
+import percent40 from "../../../assets/icons/number40.svg";
+import percent60 from "../../../assets/icons/number60.svg";
+import percent80 from "../../../assets/icons/number80.svg";
+import percent100 from "../../../assets/icons/number100.svg";
+
+export { percent20, percent40, percent60, percent80, percent100 };
