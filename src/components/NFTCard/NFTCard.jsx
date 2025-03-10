79db2cddcf364c31a6158ac26e9153609b9b9c42
Ali Akbar <sayaliakbar@gmail.com>
2025-03-11 03:19:39 +0500

    Enhance NFTCard component to accept logo and body props for improved content display

--- src/components/NFTCard/NFTCard.jsx
+++ src/components/NFTCard/NFTCard.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import "./nftcard.css";
 
-import logo from "../../assets/logo.svg";
-
-const NFTCard = ({ imageSource, title }) => {
+const NFTCard = ({ imageSource, title, logo, body }) => {
   return (
     <div className="yorfy__nftcard">
       <img className="yorfy__nftcard-image" src={imageSource} alt="{title}" />
-      <div>
-        <img src={logo} alt="logo" />
+      <div className="yorfy__nftcard-content">
+        {logo && <img src={logo} alt="logo" />}
         <h5>{title}</h5>
+        {body && <p>{body}</p>}
       </div>
     </div>
   );
