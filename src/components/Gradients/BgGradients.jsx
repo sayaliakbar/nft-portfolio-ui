2aa72773a0aa78d35e081d53ec590a42ddd35913
Ali Akbar <sayaliakbar@gmail.com>
2025-03-10 22:30:50 +0500

    Add BgGradients component to render purple and blue gradients

---
+++ src/components/Gradients/BgGradients.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./bggradients.css";
+
+const BgGradients = () => {
+  return (
+    <>
+      <div className="yorfy__gradient-purple gradient__bg-purple" />
+      <div className="yorfy__gradient-blue gradient__bg-blue" />
+    </>
+  );
+};
+
+export default BgGradients;
