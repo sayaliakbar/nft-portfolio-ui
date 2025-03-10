5fa08a2dc7c32083c5a45d55a6bee134efc0f2e7
Ali Akbar <sayaliakbar@gmail.com>
2025-03-11 03:20:09 +0500

    Add team and investor sections to About page with dynamic data rendering

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,7 +2,58 @@ import React from "react";
 
 import "./about.css";
 
-import { Heading, BgGradients, ImageCard } from "../../components/index.js";
+import {
+  sixfaces01,
+  sixfaces02,
+  sixfaces03,
+  sixfaces04,
+  sixfaces05,
+} from "./imports.js";
+
+const teamHeading = {
+  heading: "Our Team",
+  title: "Behind the Yorfy NFT",
+  body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+};
+const investorHeading = {
+  heading: "Investor",
+  title: "Special Person from Yorfy",
+  body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+};
+
+const teamData = [
+  { title: "Shoo Phar Dhie", imageSource: sixfaces01, body: "Founder" },
+  { title: "Shoo Phar Mhan", imageSource: sixfaces02, body: "Illustrator" },
+  { title: "Shoo Khier Mhan", imageSource: sixfaces03, body: "UI/UX Designer" },
+  {
+    title: "Shoo Yhan Tho",
+    imageSource: sixfaces04,
+    body: "Front End Developer",
+  },
+  {
+    title: "Shoo Mhar Nhie",
+    imageSource: sixfaces05,
+    body: "Backend Developer",
+  },
+  {
+    title: "Shoo Thar Mhan",
+    imageSource: sixfaces01,
+    body: "Quality Assurance",
+  },
+];
+
+const investorData = [
+  { title: "Mhar Yho Nho", imageSource: sixfaces01, body: "Investor" },
+  { title: "Mhar Han Tho", imageSource: sixfaces02, body: "Investor" },
+  { title: "Mhar Jhu Qhi", imageSource: sixfaces03, body: "Investor" },
+];
+
+import {
+  Heading,
+  BgGradients,
+  ImageCard,
+  Collection,
+} from "../../components/index.js";
 import { Hero } from "../../containers/About/index.js";
 
 import sixFaces01 from "../../assets/images/sixfaces01.png";
@@ -37,6 +88,11 @@ const About = () => {
               nisi ut aliquip ex ea commodo consequat.
             </>
           }
+        />
+        <Collection nftCollectionData={teamData} heading={teamHeading} />
+        <Collection
+          nftCollectionData={investorData}
+          heading={investorHeading}
         />
       </div>
     </>
