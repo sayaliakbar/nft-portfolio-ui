89b168e14e5503bd1372d33cf0a4e20205d5f100
Ali Akbar <sayaliakbar@gmail.com>
2025-03-11 03:20:37 +0500

    Add logo import to NFT assets

--- src/pages/NFT/imports.js
+++ src/pages/NFT/imports.js
@@ -3,5 +3,6 @@ import sixfaces02 from "../../assets/images/sixfaces02.png";
 import sixfaces03 from "../../assets/images/sixfaces03.png";
 import sixfaces04 from "../../assets/images/sixfaces04.png";
 import sixfaces05 from "../../assets/images/sixfaces05.png";
+import logo from "../../assets/logo.svg";
 
-export { sixfaces01, sixfaces02, sixfaces03, sixfaces04, sixfaces05 };
+export { sixfaces01, sixfaces02, sixfaces03, sixfaces04, sixfaces05, logo };
