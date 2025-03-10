e6806b3b325aecd91a8d931337f4a353522b39ab
Ali Akbar <sayaliakbar@gmail.com>
2025-03-10 22:31:11 +0500

    Remove NftBgGradients export from NFT container

--- src/containers/NFT/index.js
+++ src/containers/NFT/index.js
@@ -1,4 +1,3 @@
-import NftBgGradients from "./Gradients/NftBgGradients.jsx";
 import Benefits from "./Benefits/Benefits.jsx";
 import Timeline from "./Timeline/Timeline.jsx";
-export { NftBgGradients, Benefits, Timeline };
+export { Benefits, Timeline };
