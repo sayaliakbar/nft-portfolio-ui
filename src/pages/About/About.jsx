33da70ef75a9e8df09a7089b700c4b485eb7794a
Ali Akbar <sayaliakbar@gmail.com>
2025-03-10 23:12:29 +0500

    Add BgGradients and Heading components to About page for enhanced layout

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,8 +2,21 @@ import React from "react";
 
 import "./about.css";
 
+import { Heading, BgGradients } from "../../components/index.js";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <BgGradients />
+      <div className="yorfy__aboutUs section__margin">
+        <Heading
+          heading="Yorfy Story"
+          title="About Us"
+          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+      </div>
+    </>
+  );
 };
 
 export default About;
