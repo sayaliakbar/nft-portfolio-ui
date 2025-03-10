227ba56cb963db9bd70292bea6a33667f6958fc1
Ali Akbar <sayaliakbar@gmail.com>
2025-03-11 03:20:23 +0500

    Add logo to NFT collection data and enhance Collection component with heading and button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,13 +13,18 @@ import {
 
 import { SaleBar, Collection } from "../../components/index.js";
 
-import { sixfaces01, sixfaces02, sixfaces03 } from "./imports.js";
+import { sixfaces01, sixfaces02, sixfaces03, logo } from "./imports.js";
 
 const nftCollectionData = [
-  { title: "YorNoose #432", imageSource: sixfaces01 },
-  { title: "YorHayr #332", imageSource: sixfaces02 },
-  { title: "YorMwoth #765", imageSource: sixfaces03 },
+  { title: "YorNoose #432", imageSource: sixfaces01, logo },
+  { title: "YorHayr #332", imageSource: sixfaces02, logo },
+  { title: "YorMwoth #765", imageSource: sixfaces03, logo },
 ];
+const collectionHeading = {
+  heading: "Featued",
+  title: "Trending Collection",
+  body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+};
 
 const Home = () => {
   return (
@@ -29,7 +34,11 @@ const Home = () => {
         <Header />
         <SaleBar />
         <Featured />
-        <Collection nftCollectionData={nftCollectionData} />
+        <Collection
+          nftCollectionData={nftCollectionData}
+          heading={collectionHeading}
+          button={true}
+        />
 
         <Community />
         <SaleBar />
