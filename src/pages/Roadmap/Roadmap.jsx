fbc15e6efc475f525d70590922fcc6dcc3442e91
Ali Akbar <sayaliakbar@gmail.com>
2025-03-10 22:33:00 +0500

    Add BgGradients, Heading, and Button components to Roadmap page layout

--- src/pages/Roadmap/Roadmap.jsx
+++ src/pages/Roadmap/Roadmap.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 
 import "./roadmap.css";
+import { BgGradients, Heading, Button } from "../../components/index.js";
+import { Content } from "../../containers/Roadmap/index.js";
 
 const Roadmap = () => {
-  return <div>Roadmap</div>;
+  return (
+    <>
+      <BgGradients />
+      <div className="yorfy__roadmap">
+        <Heading
+          title="See How We Create The Yorfy"
+          heading="Roadmap"
+          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <Content />
+        <Heading
+          heading="Join Our Team"
+          title="Wan’t to Contribute Develop The Yorfy? Send Your Resume Now"
+          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <Button text="Send Your Resume" size="large" />
+      </div>
+    </>
+  );
 };
 
 export default Roadmap;
