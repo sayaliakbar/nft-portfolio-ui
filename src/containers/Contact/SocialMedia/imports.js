d21bdc56563111bac052b1e3e1268361ee7169d2
Ali Akbar <sayaliakbar@gmail.com>
2025-03-11 09:24:15 +0500

    Add imports for social media SVG icons

---
+++ src/containers/Contact/SocialMedia/imports.js
@@ -0,0 +1,6 @@
+import facebook from "../../../assets/brands/facebook.svg";
+import linkedin from "../../../assets/brands/linkedin.svg";
+import youtube from "../../../assets/brands/youtube.svg";
+import instagram from "../../../assets/brands/instagram.svg";
+
+export { facebook, linkedin, youtube, instagram };
