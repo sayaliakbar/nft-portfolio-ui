02600941c60009167652136634eeb64ac6a27783
Ali Akbar <sayaliakbar@gmail.com>
2025-03-11 06:58:37 +0500

    Refactor About page layout by replacing BgGradients with Header and adding a new container for improved structure

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -50,9 +50,9 @@ const investorData = [
 
 import {
   Heading,
-  BgGradients,
   ImageCard,
   Collection,
+  Header,
 } from "../../components/index.js";
 import { Hero } from "../../containers/About/index.js";
 
@@ -61,39 +61,40 @@ import sixFaces01 from "../../assets/images/sixfaces01.png";
 const About = () => {
   return (
     <>
-      <BgGradients />
-      <div className="yorfy__aboutUs section__margin">
-        <Heading
+      <div className="yorfy__aboutUs">
+        <Header
           heading="Yorfy Story"
           title="About Us"
           body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
-        <Hero />
-        <ImageCard
-          button={false}
-          imageSource={sixFaces01}
-          heading="History"
-          title="Born History of Yorfy NFT"
-          body={
-            <>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-              <br />
-              <br />
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </>
-          }
-        />
-        <Collection nftCollectionData={teamData} heading={teamHeading} />
-        <Collection
-          nftCollectionData={investorData}
-          heading={investorHeading}
-        />
+        <div className="yorfy__aboutUs-container section__margin">
+          <Hero />
+          <ImageCard
+            button={false}
+            imageSource={sixFaces01}
+            heading="History"
+            title="Born History of Yorfy NFT"
+            body={
+              <>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </>
+            }
+          />
+          <Collection nftCollectionData={teamData} heading={teamHeading} />
+          <Collection
+            nftCollectionData={investorData}
+            heading={investorHeading}
+          />
+        </div>
       </div>
     </>
   );
