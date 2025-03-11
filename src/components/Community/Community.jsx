0802b796c1471b7c95e538acfae3ac8e5f19d35a
Ali Akbar <sayaliakbar@gmail.com>
2025-03-11 06:56:10 +0500

    Add Community component with heading and user cards

---
+++ src/components/Community/Community.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import "./community.css";
+import { Heading, Button, UserCard } from "../index.js";
+const Community = () => {
+  return (
+    <div className="yorfy__community section__padding">
+      <div className="yorfy__community-heading">
+        <Heading
+          heading="Community"
+          title="Join Our Community and Get Many Benefits"
+          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <Button
+          text="Join Our Discord"
+          style="primary"
+          size="large"
+          onClick={() => {
+            console.log("clicked");
+          }}
+          disabled={false}
+        />
+      </div>
+      <div className="yorfy__community-userCards">
+        <UserCard />
+        <UserCard />
+        <UserCard />
+      </div>
+    </div>
+  );
+};
+
+export default Community;
