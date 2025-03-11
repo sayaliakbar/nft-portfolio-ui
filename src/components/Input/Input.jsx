f8427aeb8918a556ef1be8730f9762ec7b2fcb14
Ali Akbar <sayaliakbar@gmail.com>
2025-03-11 09:23:50 +0500

    Refactor Input component structure and CSS class naming for improved clarity

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,18 +3,21 @@ import arrow_drop_down from "../../assets/icons/arrow_drop_down.svg";
 
 const Input = ({ placeholder, label = false, size, icon = false }) => {
   return (
-    <>
+    <div className="yorfy__inputContainer">
       {label && (
-        <label className={`yorfy__label yorfy__label-${size}`} htmlFor={label}>
+        <label
+          className={`yorfy__inputContainer-label yorfy__inputContainer-label-${size}`}
+          htmlFor={label}
+        >
           {label}
         </label>
       )}
       <input
         placeholder={placeholder}
-        className={`yorfy__input yorfy__input-${size}`}
+        className={`yorfy__inputContainer-input yorfy__inputContainer-input-${size}`}
       />
       {icon && <img src={arrow_drop_down} alt="dropdown" />}
-    </>
+    </div>
   );
 };
 
