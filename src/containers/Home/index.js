0e958737b2053f661fbea5a9efdd5743b5936fa7
Ali Akbar <sayaliakbar@gmail.com>
2025-03-11 06:58:15 +0500

    Remove Community component from Home exports

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,15 +1,7 @@
 import HomeBgGradients from "./Gradients/HomeBgGradients.jsx";
 import Header from "./Header/Header.jsx";
 import Featured from "./Featured/Featured.jsx";
-import Community from "./Community/Community.jsx";
 import Collaboration from "./Collaboration/Collaboration.jsx";
 import Newsletter from "./Newsletter/Newsletter.jsx";
 
-export {
-  HomeBgGradients,
-  Header,
-  Featured,
-  Community,
-  Collaboration,
-  Newsletter,
-};
+export { HomeBgGradients, Header, Featured, Collaboration, Newsletter };
