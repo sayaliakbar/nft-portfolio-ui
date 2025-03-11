b3188a2dd422806c3de4fe144f12ded0f08160cf
Ali Akbar <sayaliakbar@gmail.com>
2025-03-12 01:27:13 +0500

    Add navigation to error page on button click in ImageCard component

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "./imagecard.css";
 import { Button, Heading } from "../index.js";
+import { useNavigate } from "react-router-dom";
 
 const ImageCard = ({ imageSource, button, heading, title, body }) => {
+  const Navigate = useNavigate();
   return (
     <div className="yorfy__imageCard section__padding">
       <div className="yorfy__imageCard-image">
         <img src={imageSource} alt={title} />
       </div>
       <div className="yorfy__imageCard-content">
-        {button && <Button text={button} />}
+        {button && <Button onClick={() => Navigate("/error")} text={button} />}
         <Heading heading={heading} title={title} body={body} />
       </div>
     </div>
