9cee217104e05d8e75aab744ea450eefc5daa98c
Ali Akbar <sayaliakbar@gmail.com>
2025-03-11 06:59:27 +0500

    Refactor Roadmap component to replace BgGradients with Header for improved structure

--- src/pages/Roadmap/Roadmap.jsx
+++ src/pages/Roadmap/Roadmap.jsx
@@ -1,28 +1,26 @@
 import React from "react";
 
 import "./roadmap.css";
-import { BgGradients, Heading, Button } from "../../components/index.js";
+import { Button, Header } from "../../components/index.js";
 import { Content } from "../../containers/Roadmap/index.js";
 
 const Roadmap = () => {
   return (
     <>
-      <BgGradients />
       <div className="yorfy__roadmap">
-        <Heading
+        <Header
           title="See How We Create The Yorfy"
           heading="Roadmap"
           body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
         <Content />
-        <Heading
+        <Header
           heading="Join Our Team"
           title="Wan’t to Contribute Develop The Yorfy? Send Your Resume Now"
           body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
         <Button text="Send Your Resume" size="large" />
       </div>
-      <BgGradients />
     </>
   );
 };
