a22c7df1ca2ab003787dface2b53bbcb08b32895
Ali Akbar <sayaliakbar@gmail.com>
2025-03-11 09:24:08 +0500

    Implement ContactForm component with input fields and submit button

--- src/containers/Contact/ContactForm/ContactForm.jsx
+++ src/containers/Contact/ContactForm/ContactForm.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 
 import "./contactform.css";
+import { Input, Button } from "../../../components/index.js";
 
 const ContactForm = () => {
-  return <div>ContactForm</div>;
+  return (
+    <form className="yorfy__contact-contactForm">
+      <div className="yorfy__contact-contactForm__name">
+        <Input placeholder="Type Here" label="First Name" size="large" />
+        <Input placeholder="Type Here" label="Last Name" size="large" />
+      </div>
+      <div className="yorfy__contact-contactForm__email">
+        <Input placeholder="Type Here" label="Email" size="large" />
+        <Input placeholder="Type Here" label="Phone Number" size="large" />
+      </div>
+      <Input placeholder="Type Here" label="Subject" size="large" />
+      <Input placeholder="Type Here" label="Message" size="large" />
+      <Button
+        text="Submit"
+        type="submit"
+        size="large"
+        onClick={(e) => {
+          e.preventDefault();
+          console.log("clicked");
+        }}
+      />
+    </form>
+  );
 };
 
 export default ContactForm;
