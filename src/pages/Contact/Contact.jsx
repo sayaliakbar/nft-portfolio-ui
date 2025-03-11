11ed1f65d5fa870ab69c3cbba62c402123e89ef8
Ali Akbar <sayaliakbar@gmail.com>
2025-03-11 06:58:05 +0500

    Update Contact component to include Header, SocialMedia, ContactForm, and Community

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 
 import "./contact.css";
+import { Header, Community } from "../../components/index.js";
+
+import { SocialMedia, ContactForm } from "../../containers/Contact/index.js";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="yorfy__contactUs">
+      <Header
+        heading="Contact Us"
+        title="Wan’t To Collaborate with Yorfy? Don’t be Shy to Talk"
+        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
+      <SocialMedia />
+      <ContactForm />
+      <Community />
+    </div>
+  );
 };
 
 export default Contact;
