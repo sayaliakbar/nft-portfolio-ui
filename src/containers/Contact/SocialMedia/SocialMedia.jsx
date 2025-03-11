8f7da9ec623c413d0fed1d6bbd5fee04372bd56a
Ali Akbar <sayaliakbar@gmail.com>
2025-03-11 09:24:37 +0500

    Implement SocialMedia component to display social media links using PointCard

--- src/containers/Contact/SocialMedia/SocialMedia.jsx
+++ src/containers/Contact/SocialMedia/SocialMedia.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 
+import { facebook, linkedin, youtube, instagram } from "./imports";
+
+import { PointCard } from "../../../components";
+
+const socialMediaData = [
+  { imageSource: facebook, title: "Yorfy Collection", body: "Facebook" },
+  { imageSource: instagram, title: "@yorfycollection", body: "Instagram" },
+  { imageSource: linkedin, title: "Yorfy Studio", body: "LinkedIn" },
+  { imageSource: youtube, title: "Life at Yorfy", body: "Youtube" },
+];
+
 import "./socialmedia.css";
 const SocialMedia = () => {
-  return <div>SocialMedia</div>;
+  return (
+    <div className="yorfy__contactUs-socialMedia">
+      {socialMediaData &&
+        socialMediaData.map((items, index) => (
+          <PointCard
+            key={index}
+            imageSource={items.imageSource}
+            title={items.title}
+            body={items.body}
+          />
+        ))}
+    </div>
+  );
 };
 
 export default SocialMedia;
