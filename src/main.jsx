1c2b404833d0c34be3f8605d05aae9432962ec36
Ali Akbar <sayaliakbar@gmail.com>
2025-03-12 22:16:57 +0500

    Import CSS files directly in main.jsx for better organization of styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+// Import all CSS files directly in JS
+import "./styles/base.css";
+import "./styles/colors.css";
+import "./styles/typography.css";
+import "./styles/components.css";
+import "./styles/layout.css";
+// You can keep this import too, if it contains any unique styles not in the above files
 import "./index.css";
 import App from "./App.jsx";
 
